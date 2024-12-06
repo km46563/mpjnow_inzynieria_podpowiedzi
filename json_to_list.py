0714200e912530aed7d327a358e7802c619b0bd3
km46563 <sirkowi3@gmail.com>
2024-12-06 16:42:20 +0100

    fixed bugs

--- json_to_list.py
+++ json_to_list.py
@@ -20,45 +20,45 @@ def extract_data(folder, output):
                     symptoms = []
                     diagnoses = []
 
-                        if 'entry' in patient_data:
-                            for entry in patient_data['entry']:
-                                resource = entry.get('resource', {})
+                    if 'entry' in patient_data:
+                        for entry in patient_data['entry']:
+                            resource = entry.get('resource', {})
 
-                                # Dane pacjenta
-                                if resource.get('resourceType') == 'Patient':
-                                    if 'name' in resource:
-                                        names = resource['name'][0]
-                                        first_name = names.get('given', [""])[0]
-                                        last_name = names.get('family', "Nieznane")
-                                        name = f'{first_name, last_name}'
+                            # Dane pacjenta
+                            if resource.get('resourceType') == 'Patient':
+                                if 'name' in resource:
+                                    names = resource['name'][0]
+                                    first_name = names.get('given', [""])[0]
+                                    last_name = names.get('family', "Nieznane")
+                                    name = f'{first_name, last_name}'
 
-                                # Diagnozy 
-                                if resource.get('resourceType') == 'Condition':
-                                    if 'code' in resource:
-                                        diagnosis = resource['code'].get('text', 'Brak diagnozy')
-                                        diagnoses.append(diagnosis)
+                            # Diagnozy
+                            if resource.get('resourceType') == 'Condition':
+                                if 'code' in resource:
+                                    diagnosis = resource['code'].get('text', 'Brak diagnozy')
+                                    diagnoses.append(diagnosis)
 
-                                # Symptomy 
-                                if resource.get('resourceType') == 'Observation':
-                                    if 'code' in resource:
-                                        observation_text = resource['code'].get('text', None)
-                                        if observation_text:
-                                            symptoms.append(observation_text)
-                                    if 'valueString' in resource:
-                                        symptoms.append(resource['valueString'])
+                            # Symptomy
+                            if resource.get('resourceType') == 'Observation':
+                                if 'code' in resource:
+                                    observation_text = resource['code'].get('text', None)
+                                    if observation_text:
+                                        symptoms.append(observation_text)
+                                if 'valueString' in resource:
+                                    symptoms.append(resource['valueString'])
 
-                        # Formatowanie list 
-                        symptoms = '; '.join(set(symptoms))
-                        diagnoses = '; '.join(set(diagnoses))
+                    # Formatowanie list
+                    symptoms = '; '.join(set(symptoms))
+                    diagnoses = '; '.join(set(diagnoses))
 
-                        data.append([name, symptoms, diagnoses])
+                    data.append([name, symptoms, diagnoses])
 
                 except json.JSONDecodeError as e:
-                    print(f"Błąd dekodowania pliku {filename}: {e}")
+                    print(f"Błąd dekodowania JSON w pliku {filename}: {e}")
 
     with open(output, 'w', newline='', encoding='utf8') as csvfile:
         csvwriter = csv.writer(csvfile)
-        csvwriter.writerow(data)
+        csvwriter.writerows(data)
 
     print("Zapisano dane pacjenta")
 
